6d6c70ba67f2aee4a1f17c9dcdea043e9aafbe44
skotecki <sergiusz.kotecki@itsystems.biz.pl>
2017-12-19 19:21:01 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+import model.OrderItem;
+
+public class Main {
+
+    public static void main(String[] args) {
+        OrderItem test = new OrderItem();
+        System.out.println("test");
+    }
+}
---
+++ src/main/java/model/OrderItem.java
@@ -0,0 +1,9 @@
+package model;
+
+public class OrderItem {
+
+    int netPrice;
+    int quantity;
+    int netTotal;
+    int total;
+}
