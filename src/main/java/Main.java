63032591bd3de35f1a873e06bef94b60305c1ff6
skotecki <sergiusz.kotecki@itsystems.biz.pl>
2017-12-19 20:59:32 +0100

    code cleanup, add calculate methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,8 +3,7 @@ import model.OrderItem;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("test");
         OrderItem test = new OrderItem(1, 12, 10, 5);
-        System.out.println(test.toString());
+
     }
 }
--- src/main/java/model/Order.java
+++ src/main/java/model/Order.java
@@ -1,17 +1,45 @@
 package model;
 
+import lombok.Data;
+
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
-
+@Data
 public class Order {
-    List<OrderItem> orderedItems = new ArrayList<>();
 
-    public void addItemToCart (OrderItem item) {
+    List<OrderItem> orderedItems = new ArrayList<>();
+    private double netTotal;
+    private double tax;
+    private double total;
+
+    public Order(List<OrderItem> orderedItems) {
+        this.orderedItems = orderedItems;
+        this.netTotal = calculateNetTotal(orderedItems);
+        this.total = calculateGrosTotal(orderedItems);
+        this.tax = calculateTaxValue(this.total, this.netTotal);
+    }
+
+    private double calculateTaxValue(double total, double netTotal) {
+        return total - netTotal;
+    }
+
+    private double calculateGrosTotal(List<OrderItem> orderedItems) {
+        return orderedItems.stream().mapToDouble(y -> y.getTotal()).sum();
+    }
+
+
+    private double calculateNetTotal(List<OrderItem> orderedItems) {
+        return orderedItems.stream().mapToDouble(w -> w.getNetTotal()).sum();
+    }
+
+    public void addItemToCart(OrderItem item) {
         orderedItems.add(item);
     }
 
-    public void deleteItem (OrderItem item) {
+    public void deleteItem(OrderItem item) {
         orderedItems.remove(item);
     }
 
